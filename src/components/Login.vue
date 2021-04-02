fa11f8ff812ea11cce625b3be6c9f7fe91632b95
Charlie-dev-test <mailbox.boyko@gmail.com>
2021-04-02 15:39:29 +0300

    add login form

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,9 +1,44 @@
 <template>
     <form action="" class="form-login" @submit.prevent="logIn()">
         <label for="login" class="form-label align-self-start">Логин</label>
-        <input class="form-control mb-3" type="text" id="login">
+        <input
+                class="form-control mb-3"
+                type="text"
+                id="login"
+                v-model.trim="login"
+                @blur="$v.login.$touch()"
+                :class="{'is-invalid': $v.login.$error}"
+        >
+        <div class="invalid-feedback" v-if="!$v.login.required">
+            Пожалуйста, укажите логин
+        </div>
+        <div class="invalid-feedback" v-if="!$v.login.minLength">
+            Не менее {{$v.login.$params.minLength.min}} символов
+        </div>
+        <div class="invalid-feedback" v-if="!$v.login.maxLength">
+            Не более {{$v.login.$params.maxLength.max}} символов
+        </div>
         <label for="login-pass" class="form-label align-self-start">Пароль</label>
-        <input class="form-control mb-3" type="password" id="login-pass">
+        <input
+                class="form-control mb-3"
+                type="password"
+                id="login-pass"
+                v-model="pass"
+                @blur="$v.pass.$touch()"
+                :class="{'is-invalid': $v.pass.$error}"
+        >
+        <div class="invalid-feedback" v-if="!$v.pass.required">
+            Пожалуйста, укажите пароль
+        </div>
+        <div class="invalid-feedback" v-if="!$v.pass.minLength">
+            Не менее {{$v.pass.$params.minLength.min}} символов
+        </div>
+        <div class="invalid-feedback" v-if="!$v.pass.maxLength">
+            Не более {{$v.pass.$params.maxLength.max}} символов
+        </div>
+        <div class="invalid-feedback" v-if="!$v.pass.valid">
+            Строчные,заглавные латинские буквы, спецсимволы и цифры
+        </div>
         <div class="modal-footer">
             <button
                     type="button"
@@ -18,26 +53,46 @@
 </template>
 
 <script>
-    // import { required, email} from 'vuelidate/lib/validators'
+    import { required, minLength, maxLength} from 'vuelidate/lib/validators'
     export default {
         name: "Login",
         data() {
             return {
-                email: '',
+                login: '',
+                pass: ''
             }
         },
-        // validations: {
-        //     email: {
-        //         required: required,
-        //         email: email
-        //     }
-        // },
+        validations: {
+            login: {
+                required: required,
+                minLength: minLength(5),
+                maxLength: maxLength(25)
+            },
+            pass: {
+                required: required,
+                minLength: minLength(5),
+                maxLength: maxLength(25),
+                valid: function(value) {
+                    const containsUppercase = /[A-Z]/.test(value);
+                    const containsLowercase = /[a-z]/.test(value);
+                    const containsNumber = /[0-9]/.test(value);
+                    const containsSpecial = /[#?!@$%^&*-]/.test(value);
+                    return containsUppercase && containsLowercase && containsNumber && containsSpecial
+                }
+            },
+        },
         methods: {
             close() {
                 this.$emit('closeLogin')
             },
             logIn() {
-                console.log('login')
+                this.$v.login.$touch();
+                this.$v.pass.$touch();
+                if(!this.$v.login.$error){
+                    if(!this.$v.pass.$error){
+                        this.$emit('closeLogin');
+                    }
+                }
             }
         }
     }
